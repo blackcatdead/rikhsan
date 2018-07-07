cf71159c2072e1faac3a9a62ef96d858b92eae5b
rikhsan <rakhmad.ikhsanudin@gmail.com>
2018-07-08 05:50:26 +0700

    + celery + tasks

---
+++ sucker/tasks.py
@@ -0,0 +1,35 @@
+
+from __future__ import absolute_import, unicode_literals
+from celerybeat.celery import app
+# from django_celery_results.models import TaskResult
+
+
+@app.task
+def add(x, y):
+    return x + y
+
+
+@app.task
+def mul(x, y):
+    return x * y
+
+
+@app.task
+def xsum(numbers):
+    return sum(numbers)
+
+@app.task
+def xsum2(numbers):
+    return sum(numbers)
+
+@app.task
+def topic_kompas(numbers):
+	# TaskResult.objects.all().delete()
+	rom sucker.grab.kompas import visitTopic
+	return visitTopic(url,limit)
+	return 1
+
+@app.task
+def topic_liputan6(url,limit):
+	from sucker.grab.liputan6 import visitTopic
+	return visitTopic(url,limit)
